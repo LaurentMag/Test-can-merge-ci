fcf3ffb5ba1697294a6c8f245b8b9a14583b3d83
Laurent <magrez0laurent@gmail.com>
2024-11-25 15:15:39 +0100

    initial commit main and start

---
+++ test.ts
@@ -0,0 +1,15 @@
+const displayDateReadOnly = (
+  value: string | null | undefined,
+  formatter = "dd/MM/yyyy, HH:mm",
+): string | null | undefined => {
+  if (!value) return value;
+
+  const date = new Date(value);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+  const year = date.getFullYear();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+
+  return `${day}/${month}/${year}, ${hours}:${minutes}`;
+};
