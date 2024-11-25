543a5ceb56b12f8c01acc80d905aff47287a0e47
Laurent <magrez0laurent@gmail.com>
2024-11-25 17:00:19 +0100

    function update

--- test.ts
+++ test.ts
@@ -4,6 +4,8 @@ const displayDateReadOnly = (
 ): string | null | undefined => {
   if (!value) return value;
 
+  console.log('formatter', formatter);
+
   const date = new Date(value);
   const day = String(date.getDate()).padStart(2, '0');
   const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
